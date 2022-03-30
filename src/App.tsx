d57455a2eed9a4aefce0012a4bb92b7af168ee32
Fata Sefer <sefer.fata@gmail.com>
2022-03-30 18:20:27 +0200

    ui: update header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import "antd/dist/antd.css";
 
 import { Layout, PageHeader, Button } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
 
 const { Footer, Content } = Layout;
 
 const App = () => {
   return (
     <Layout>
-      <PageHeader className="site-page-header" title="Password Generator" />
+      <PageHeader
+        className="site-page-header"
+        title="Password Generator"
+        subTitle="Fata Sefer"
+        extra={[
+          <Button
+            key="3"
+            type="link"
+            href="https://github.com/fs98/password-generator"
+            target="_blank"
+          >
+            <GithubOutlined style={{ fontSize: "24px", color: "#08c" }} />
+          </Button>,
+        ]}
+      />
       <Content>Content</Content>
       <Footer>Footer</Footer>
     </Layout>
