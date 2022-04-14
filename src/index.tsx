080b44838f4df4804135082e78319ea9362296e3
Fata Sefer <sefer.fata@gmail.com>
2022-04-14 17:35:39 +0200

    chore: upgrade to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLDivElement
+);
+root.render(<App />);
