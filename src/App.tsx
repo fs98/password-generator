3ba27d3175ecb4fda616a2cde97ef0ef6b6f536c
Fata Sefer <sefer.fata@gmail.com>
2022-04-17 01:51:44 +0200

    refactor: specify return type on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "antd/dist/antd.min.css";
 
 import { Layout, PageHeader, Button } from "antd";
@@ -5,28 +6,28 @@ import { GithubOutlined } from "@ant-design/icons";
 
 const { Footer, Content } = Layout;
 
-const App = () => {
-  return (
-    <Layout>
-      <PageHeader
-        className="site-page-header"
-        title="Password Generator"
-        subTitle="Fata Sefer"
-        extra={[
-          <Button
-            key="3"
-            type="link"
-            href="https://github.com/fs98/password-generator"
-            target="_blank"
-          >
-            <GithubOutlined className="text-2xl" />
-          </Button>,
-        ]}
-      />
-      <Content>Content</Content>
-      <Footer className="text-center">Password Generater ©2022</Footer>
-    </Layout>
-  );
-};
+function App(): JSX.Element {
+    return (
+        <Layout>
+            <PageHeader
+                className="site-page-header"
+                title="Password Generator"
+                subTitle="Fata Sefer"
+                extra={[
+                    <Button
+                        key="3"
+                        type="link"
+                        href="https://github.com/fs98/password-generator"
+                        target="_blank"
+                    >
+                        <GithubOutlined className="text-2xl" />
+                    </Button>,
+                ]}
+            />
+            <Content>Content</Content>
+            <Footer className="text-center">Password Generater ©2022</Footer>
+        </Layout>
+    );
+}
 
 export default App;
