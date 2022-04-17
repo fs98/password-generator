2baa86cffad05a489f458acc653169686f46efe5
Fata Sefer <sefer.fata@gmail.com>
2022-04-18 00:19:54 +0200

    ui: create initial form

--- src/components/PasswordGenerator.tsx
+++ src/components/PasswordGenerator.tsx
@@ -1,5 +1,56 @@
 import React from 'react';
+import { Form, Input, Button, Card } from 'antd';
+import type { FormProps } from 'antd';
 
 export const PasswordGenerator = (): JSX.Element => {
-  return <div>Password Generator</div>;
+  const onFinish = (values: FormProps): void => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any): void => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Card>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: false,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 4,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Generate Password
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 };
