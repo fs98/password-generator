dfc7f7747f9908889c2961c62e70ce6f47d37c55
Fata Sefer <sefer.fata@gmail.com>
2022-04-17 02:22:23 +0200

    refactor: format the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import "antd/dist/antd.min.css";
+import React from 'react';
+import 'antd/dist/antd.min.css';
 
-import { Layout, PageHeader, Button } from "antd";
-import { GithubOutlined } from "@ant-design/icons";
+import { Layout, PageHeader, Button } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 
 const { Footer, Content } = Layout;
 
-function App(): JSX.Element {
-    return (
-        <Layout>
-            <PageHeader
-                className="site-page-header"
-                title="Password Generator"
-                subTitle="Fata Sefer"
-                extra={[
-                    <Button
-                        key="3"
-                        type="link"
-                        href="https://github.com/fs98/password-generator"
-                        target="_blank"
-                    >
-                        <GithubOutlined className="text-2xl" />
-                    </Button>,
-                ]}
-            />
-            <Content>Content</Content>
-            <Footer className="text-center">Password Generater ©2022</Footer>
-        </Layout>
-    );
-}
+const App = (): JSX.Element => {
+  return (
+    <Layout>
+      <PageHeader
+        className="site-page-header"
+        title="Password Generator"
+        subTitle="Fata Sefer"
+        extra={[
+          <Button
+            key="3"
+            type="link"
+            href="https://github.com/fs98/password-generator"
+            target="_blank"
+          >
+            <GithubOutlined className="text-2xl" />
+          </Button>,
+        ]}
+      />
+      <Content>Content</Content>
+      <Footer className="text-center">Password Generater ©2022</Footer>
+    </Layout>
+  );
+};
 
 export default App;
