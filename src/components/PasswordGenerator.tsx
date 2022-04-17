f319fba3088b8fd7d0b4b5a0f363882082c42964
Fata Sefer <sefer.fata@gmail.com>
2022-04-18 01:09:59 +0200

    create initial logic for password generating

--- src/components/PasswordGenerator.tsx
+++ src/components/PasswordGenerator.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Form, Input, Button, Card } from 'antd';
+import React, { useState } from 'react';
+import { Form, Button, Card, Alert } from 'antd';
 import type { FormProps } from 'antd';
 
 export const PasswordGenerator = (): JSX.Element => {
+  const [password, setPassword] = useState('Not Generated');
+
   const onFinish = (values: FormProps): void => {
-    console.log('Success:', values);
+    // console.log('Success:', values);
+
+    const chars =
+      '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const passwordLength = 12;
+    let generatedPassword = '';
+
+    for (let i = 0; i <= passwordLength; i += 1) {
+      const randomNumber = Math.floor(Math.random() * chars.length);
+      generatedPassword += chars.substring(randomNumber, randomNumber + 1);
+
+      setPassword(generatedPassword);
+    }
   };
 
   const onFinishFailed = (errorInfo: any): void => {
@@ -12,7 +26,7 @@ export const PasswordGenerator = (): JSX.Element => {
   };
 
   return (
-    <Card>
+    <Card className="h-full">
       <Form
         name="basic"
         labelCol={{
@@ -29,15 +43,12 @@ export const PasswordGenerator = (): JSX.Element => {
         autoComplete="off"
       >
         <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: false,
-            },
-          ]}
+          wrapperCol={{
+            offset: 4,
+            span: 16,
+          }}
         >
-          <Input />
+          <Alert message={password} type="info" />
         </Form.Item>
 
         <Form.Item
@@ -48,6 +59,9 @@ export const PasswordGenerator = (): JSX.Element => {
         >
           <Button type="primary" htmlType="submit">
             Generate Password
+          </Button>
+          <Button htmlType="button" className="ml-3" disabled>
+            Copy Password
           </Button>
         </Form.Item>
       </Form>
